82f2f8002171421e8fcd48e8bfd217870de3318c
AlexBoXY <78037878+AlexBoXY@users.noreply.github.com>
2022-04-18 19:14:55 +0200

    Add copyright notice

--- move_maindir.sh
+++ move_maindir.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+# Copyright (C) 2022  Alexander Boldt <alexboldt@yahoo.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 dir="$1/"
 ext=$(kdialog --checklist "Select File-Extension(s):" 1 "*.mkv" on 2 "*.avi" off 3 "*.mp4" off)
 rcExt=$?
@@ -18,3 +33,4 @@ if [[ $rcExt == 0 ]]; then
 	done
 fi
 kdialog --passivepopup "Finished Move MainDir"
+
