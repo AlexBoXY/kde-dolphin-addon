1b7fe90ee4b7770c7c9847d63f58787a59b9c22f
AlexBoXY <78037878+AlexBoXY@users.noreply.github.com>
2022-04-18 19:13:35 +0200

    Chanage name of config-file, add copyright notice

--- archiv_extract.sh
+++ archiv_extract.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-# Author AlexBoXY
-# licened under GPLv3
+# Copyright (C) 2022  Alexander Boldt <alexboldt@yahoo.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 where=$(dirname "$@")
 cd "$where"
 # Passwords must be in file ~/.config/archiv_extract.pass  (one password per line)
-PASSWORDS="$HOME/.config/archiv_extract.pass"
+PASSWORDS="$HOME/.config/archiv_extract.conf"
 dir=$(kdialog --getexistingdirectory ~/);
 if [[ $? == 0 ]]; then
 	for i in $@; do
