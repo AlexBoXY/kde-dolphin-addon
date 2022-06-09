da0ec090a9b0470e3014ca071f8b42b33aa24926
Alexander Boldt <alexanderboldt0@gmail.com>
2022-06-09 08:02:17 +0200

    Add Dunstify Reduce Notifications
    use Dunstify instead of kdialog
    Reduced number of notifications

--- move_maindir.sh
+++ move_maindir.sh
@@ -17,20 +17,21 @@
 dir="$1/"
 ext=$(kdialog --checklist "Select File-Extension(s):" 1 "*.mkv" on 2 "*.avi" off 3 "*.mp4" off)
 rcExt=$?
+count=0
 if [[ $rcExt == 0 ]]; then
 	for i in $ext; do
 		case $i in
-			'"1"' )
-                find $dir -mindepth 2 -name '*.mkv' -exec mv {} $dir \; -exec kdialog --passivepopup {} \;
-                ;;
-			'"2"' )
-                find $dir -mindepth 2 -name '*.avi' -exec mv {} $dir \; -exec kdialog --passivepopup {} \;
-                ;;
-			'"3"' )
-                find $dir -mindepth 2 -name '*.mp4' -exec mv {} $dir \; -exec kdialog --passivepopup {} \;
-                ;;
+		*1* )
+			count=$(expr $count + $(find $dir -mindepth 2 -name '*.mkv' -exec mv {} $dir \; -printf '.' | wc -c))
+			;;
+		*2* )
+			count=$(expr $count + $(find $dir -mindepth 2 -name '*.avi' -exec mv {} $dir \; -printf '.' | wc -c))
+			;;
+		*3* )
+			count=$(expr $count + $(find $dir -mindepth 2 -name '*.mp4' -exec mv {} $dir \;  -printf '.' | wc -c))
+			;;
 		esac
 	done
 fi
-kdialog --passivepopup "Finished Move MainDir"
+dunstify -a "move_mainDir" -u low -i dialog-information "Finished moving $count files."
 
