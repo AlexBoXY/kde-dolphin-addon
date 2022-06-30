312dac02d81c1b4d63c43b9e4c7ffdafda028255
alexander <alexanderboldt0@gmail.com>
2022-06-30 22:51:54 +0200

    Change move_mainDir use .conf

--- move_maindir.sh
+++ move_maindir.sh
@@ -17,25 +17,9 @@
 # Filetypes must be in file ~/.config/move_mainDir.conf  (one file type per line with wildcard '*' eg. *.txt ).
 CONFIG="$HOME/.config/move_mainDir.conf"
 dir="$1/"
-#ext=$(kdialog --checklist "Select File-Extension(s):" 1 "*.mkv" on 2 "*.avi" off 3 "*.mp4" off)
-rcExt=$?
 count=0
-if [[ $rcExt == 0 ]]; then
-#	for i in $ext; do
-    while read -r ftyp; do
-#		case $i in
-#		*1* )
-echo $ftyp
-			count=$(expr $count + $(find $dir -mindepth 2 -name $ftyp -exec mv {} $dir \; -printf '.' | wc -c))
-#			;;
-#		*2* )
-#			count=$(expr $count + $(find $dir -mindepth 2 -name '*.avi' -exec mv {} $dir \; -printf '.' | wc -c))
-#			;;
-#		*3* )
-#			count=$(expr $count + $(find $dir -mindepth 2 -name '*.mp4' -exec mv {} $dir \;  -printf '.' | wc -c))
-#			;;
-#		esac
-	done < $CONFIG
-fi
+while read -r ftyp; do
+    count=$(expr $count + $(find $dir -mindepth 2 -name "$ftyp" -exec mv {} $dir \; -printf '.' | wc -c))
+done < $CONFIG
 dunstify -a "move_mainDir" -u low -i dialog-information "Finished moving $count files."
 
